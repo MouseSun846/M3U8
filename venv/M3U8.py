58c879d3d8543e7c40b0ca1d6c12dc5acd636259
艾勒先生 <3026098675@qq.com>
2020-01-31 22:27:34 +0800

    下一站是幸福(电视剧下载)

--- venv/M3U8.py
+++ venv/M3U8.py
@@ -1,31 +1,26 @@
 import os
 import requests
 import re
-
-
-def download(url,counter):
-    baseDir = "down/"
+def download(url):
+    baseDir = "down/10"
     r = requests.get(url)
-    with open(baseDir+str(counter)+".ts", "wb") as tsf:
+    with open(baseDir+".mp4", "ab+") as tsf:
          tsf.write(r.content)
 def parseUrlContent(file,counter,head):
     if len(file.text)>0:
         res = re.findall(".*?\.ts",file.text)
         mlist=[]
         for line in res:
-            mlist.append(head[0]+line)
-        # print(mlist)
+            mlist.append(head+line)
         for line in mlist:
             counter = counter + 1
-            download(line,counter)
+            download(line)
             print("downloading file ("+str(counter+1)+"/"+str(len(mlist))+")")
-url = "https://m3u8.cnkamax.com/useruploadfiles/e189b564e09b5b62a660234ce395d957.m3u8"
-nurl = url.replace("https://m3u8","https://p")
-head = re.split("\w+\.m3u8",nurl)
-file = requests.get(nurl)
+        print("下载完成！")
+url = "https://youku.cdn2-okzy.com/20200130/7452_f33dce52/index.m3u8"
+url1="http://cn7.qxreader.com/hls/20200130/0e4ebab6b8156d7f2e6408598ee3428a/1580396818/index.m3u8"
+url2="http://cn7.qxreader.com/hls/20200131/61dd5801c4991eab7f953d9bb3b6b572/1580456347/index.m3u8"
+head = "http://cn7.qxreader.com"
+file = requests.get(url2)
 counter = 0
-if file.status_code == 200:
-    parseUrlContent(file,counter,head)
-else:
-    file = requests.get(url)
-    parseUrlContent(file,counter,head)
+parseUrlContent(file,counter,head)
